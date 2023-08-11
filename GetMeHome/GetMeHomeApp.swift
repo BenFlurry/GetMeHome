b1b2b3c7cd86eab7214950e999c5615809fd0b66
Ben Alexander <benjaminalexander747@gmail.com>
2023-08-11 13:40:03 +0100

    Initial Commit

---
+++ GetMeHome/GetMeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMeHomeApp.swift
+//  GetMeHome
+//
+//  Created by Benjamin Alexander on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct GetMeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
